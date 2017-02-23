7662452cf6a2d4dc98e71c0cb34f9827033f3a1e
WJLIDDY <wjl36@case.edu>
2017-02-23 14:06:23 -0500

    Main Menu Screen

---
+++ Songbook/Songbook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Songbook
+//
+//  Created by William Liddy on 2/23/17.
+//  Copyright © 2017 NeutralSpace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
